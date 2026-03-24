7dba951113c6f4bccc4f3a954b0088a5faba01eb
Jules2762 <jules.haisoa@gmail.com>
2026-03-24 08:28:31 +0100

    ajout supabae initialisation

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Campus insight",
       debugShowCheckedModeBanner: false,
       themeAnimationCurve: Curves.elasticIn,
       initialRoute: '/',
